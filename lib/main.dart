eb455f0756d60f0424fa29a166bc16b456ab9640
Roman Jaquez <romanjaquez@Romans-MacBook-Pro.local>
2019-03-15 08:31:59 -0400

    fonts and scrolling effect

--- lib/main.dart
+++ lib/main.dart
@@ -20,15 +20,13 @@ class TimelinePage extends StatefulWidget {
 }
 
 class TimelinePageState extends State<TimelinePage> {
-  List<HistoryEntry> historyEntries;
+  List<HistoryEntry> historyEntries; String someValue = ''; double itemSize = 180; int rowOffset = 3;
+  ScrollController _scrollController =ScrollController();
   TextStyle labelStyle = TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 10);
   TextStyle labelStyleBold = TextStyle(fontWeight:FontWeight.bold, color: Colors.black.withOpacity(0.5), fontSize: 10);
   Color lightGrey = Colors.grey[300]; Color presidencyColor = Colors.lightBlue; Color eventColor = Colors.redAccent;
 
-  Future loadHistoryEntries() async {
-    String jsonString = await rootBundle.loadString('assets/events.json');
-    setState(() { historyEntries = List<HistoryEntry>(); json.decode(jsonString).forEach((e) { historyEntries.add(HistoryEntry.toJson(e)); }); });
-  }
+  Future loadHistoryEntries() async { String jsonString = await rootBundle.loadString('assets/events.json'); setState(() { historyEntries = List<HistoryEntry>(); json.decode(jsonString).forEach((e) { historyEntries.add(HistoryEntry.toJson(e)); }); });}
 
   BoxDecoration circleDecoration(Color color) { return BoxDecoration(shape: BoxShape.circle, color: color); }
   void initState() { super.initState(); Future.delayed(Duration(milliseconds: 2000), () { loadHistoryEntries(); });}
@@ -37,26 +35,18 @@ class TimelinePageState extends State<TimelinePage> {
   Widget build(BuildContext context) {
     return Scaffold(body: historyEntries != null ? Stack(
         children: <Widget>[
-          Center(child: Opacity(opacity: 0.1,child: Image.asset('assets/us_map.png', width: 200, height: 200))),
+          Center(child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[ Opacity(opacity: 0.1,child: Image.asset('assets/us_map.png', width: 200, height: 150)),Text(someValue, style:TextStyle(fontFamily: 'TraditioAh', color: Colors.grey.withOpacity(0.5), fontSize: 70))])),
           Container(margin: EdgeInsets.only(left: 37),color: Colors.grey.withOpacity(0.5),width: 1),
-          Container(child: ListView.builder(
-              itemCount: historyEntries.length, itemBuilder: (context, index) {
-                return Container(height: 180,padding: EdgeInsets.only(left: 30),
-                  child: Stack( children: <Widget>[
+          Container(child: NotificationListener(child: ListView.builder(controller: _scrollController, itemExtent: 180,
+              itemCount: historyEntries.length, itemBuilder: (context, index) { return Container(height: 180,padding: EdgeInsets.only(left: 30),child: Stack( children: <Widget>[
                       Stack(children: <Widget>[Container(margin: index == 0 ? EdgeInsets.only(top: 0) : EdgeInsets.only(top: 150),child: Container(width: 15,height: 15,decoration: this.circleDecoration((index == 0 || index == historyEntries.length - 1) ? Colors.lightGreen : Colors.transparent)))],),
                       Positioned(top: 65,child: Stack(children: <Widget>[
                             Container(margin:EdgeInsets.only(top: 6, left: 3),color: historyEntries[index].eventType == 'presidency' ? presidencyColor : eventColor, height: 2, width: historyEntries[index].eventType == 'presidency' ? 210 : 110),
                             Container(width: 15,height: 15,decoration: this.circleDecoration(historyEntries[index].eventType == 'presidency' ? presidencyColor : eventColor))])),
-                      Positioned(left: historyEntries[index].eventType == 'presidency' ? 180 : 80, top: 30, child: Column(children: <Widget>[ GestureDetector(onTap: () async { await launch(historyEntries[index].link); }, child: Stack(children: <Widget>[Container(width: 85, height: 85, decoration: this.circleDecoration(historyEntries[index].eventType == 'presidency' ? presidencyColor : eventColor)),
-                              Positioned(top: 2.5, left: 2.5, child: ClipOval(child: Container(width: 80,height: 80,child: Image.asset('assets/' + historyEntries[index].imgPath + '.jpg', fit:BoxFit.cover, width: 70, height: 70))))])),Container(width: 130,margin:EdgeInsets.only(top: 5),child: Column( children: <Widget>[
-                                  Text(DateFormat.yMMMd('en_US').format(DateTime.parse(historyEntries[index].year)), style: labelStyleBold, textAlign: TextAlign.center),
-                                  Text(historyEntries[index].name + '\n' + historyEntries[index].description, style: labelStyle, textAlign: TextAlign.center)],
-                              ))
-                          ])
-                      )],
+                            Positioned(left: historyEntries[index].eventType == 'presidency' ? 180 : 80, top: 30, child: Column(children: <Widget>[ GestureDetector(onTap: () async { await launch(historyEntries[index].link); }, child: Stack(children: <Widget>[Container(width: 85, height: 85, decoration: this.circleDecoration(historyEntries[index].eventType == 'presidency' ? presidencyColor : eventColor)),
+                            Positioned(top: 2.5, left: 2.5, child: ClipOval(child: Container(width: 80,height: 80,child: Image.asset('assets/' + historyEntries[index].imgPath + '.jpg', fit:BoxFit.cover, width: 70, height: 70))))])),Container(width: 130,margin:EdgeInsets.only(top: 5),child: Column( children: <Widget>[Text(DateFormat.yMMMd('en_US').format(DateTime.parse(historyEntries[index].year)), style: labelStyleBold, textAlign: TextAlign.center),Text(historyEntries[index].name + '\n' + historyEntries[index].description, style: labelStyle, textAlign: TextAlign.center)],))]))],
                   ));
-              }),  
-          )]) : Center(child: Column(crossAxisAlignment: CrossAxisAlignment.center,mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[Opacity(child: Image.asset('assets/history_logo.png', width: 200, height: 200), opacity: 0.5),CircularProgressIndicator(valueColor: AlwaysStoppedAnimation(Colors.grey))]))
+              }), onNotification: (t) { setState(() { someValue = DateFormat.y('en_US').format(DateTime.parse(historyEntries[(_scrollController.offset ~/ itemSize).toInt() + rowOffset].year)); });}))]) : Center(child: Column(crossAxisAlignment: CrossAxisAlignment.center,mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[Opacity(child: Image.asset('assets/history_logo.png', width: 200, height: 200), opacity: 0.5),CircularProgressIndicator(valueColor: AlwaysStoppedAnimation(Colors.grey))]))
     );
   }
 }
